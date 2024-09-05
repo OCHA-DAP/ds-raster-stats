30718d4f5f747d7432dad43af48cfd2c30c89e55
hannahker <hannah.ker96@gmail.com>
2024-09-05 14:52:26 -0700

    Update era5 test config

--- config.py
+++ config.py
@@ -16,9 +16,9 @@ DATASETS = {
         "coverage": "global",
         "update_schedule": "0 0 6 * *",
         "dev_run": {
-            "start_date": "2020-01-01",
-            "end_date": "2020-01-01",
-            "iso3s": ["MSR"],
+            "start_date": "1981-01-01",
+            "end_date": "2024-07-30",  # TODO
+            "iso3s": ["YEM"],
         },
     },
     "imerg": {

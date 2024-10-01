332f163094f2c5dcba1c7383b02e42d7b1ac4d32
hannahker <hannah.ker96@gmail.com>
2024-10-01 09:48:24 -0700

    Split processing into yearly chunks

--- run_raster_stats.py
+++ run_raster_stats.py
@@ -13,12 +13,56 @@ from src.utils.database_utils import (
     db_engine,
     insert_qa_table,
 )
+from src.utils.general_utils import split_date_range
 from src.utils.inputs import cli_args
 from src.utils.iso3_utils import create_iso3_df, get_iso3_data, load_shp
 from src.utils.raster_utils import fast_zonal_stats_runner, prep_raster
 
 logger = logging.getLogger(__name__)
 coloredlogs.install(level=LOG_LEVEL, logger=logger)
+
+
+def process_chunk(start, end, dataset, args, engine, df_iso3s):
+    logger.info(f"Processing data from {start} to {end}")
+    ds = stack_cogs(start, end, dataset, args.mode)
+
+    for idx, row in df_iso3s.iterrows():
+        iso3 = row["iso_3"]
+        shp_url = row["o_shp"]
+        max_adm = row["max_adm_level"]
+        logger.info(f"Processing data for {iso3}...")
+
+        with tempfile.TemporaryDirectory() as td:
+            load_shp(shp_url, td, iso3)
+            gdf = gpd.read_file(f"{td}/{iso3.lower()}_adm0.shp")
+            try:
+                ds_clipped = prep_raster(ds, gdf)
+            except Exception as e:
+                logger.error(f"Error preparing raster for {iso3}: {e}")
+                stack_trace = traceback.format_exc()
+                insert_qa_table(iso3, None, dataset, e, stack_trace, engine)
+                continue
+
+            for adm_level in range(max_adm + 1):
+                try:
+                    gdf = gpd.read_file(f"{td}/{iso3.lower()}_adm{adm_level}.shp")
+                    logger.info(f"Computing stats for adm{adm_level}...")
+                    fast_zonal_stats_runner(
+                        ds_clipped,
+                        gdf,
+                        adm_level,
+                        iso3,
+                        save_to_database=True,
+                        engine=engine,
+                        dataset=dataset,
+                    )
+                except Exception as e:
+                    logger.error(
+                        f"Error calculating stats for {iso3} at {adm_level}: {e}"
+                    )
+                    stack_trace = traceback.format_exc()
+                    insert_qa_table(iso3, adm_level, dataset, e, stack_trace, engine)
+                    continue
 
 
 if __name__ == "__main__":
@@ -37,49 +81,14 @@ if __name__ == "__main__":
 
     sel_iso3s = settings["test"]["iso3s"] if args.test else None
     df_iso3s = get_iso3_data(sel_iso3s, engine)
+    date_ranges = split_date_range(start, end)
 
-    logger.info(f"Creating stack of COGs from {start} to {end}...")
-    ds = stack_cogs(start, end, dataset, args.mode)
+    if len(date_ranges) > 1:
+        logger.info(f"Processing data in {len(date_ranges)} chunks")
+    else:
+        logger.info("Processing entire date range in a single chunk")
 
-    # Loop through each country
-    logger.info(f"Calculating raster stats for {len(df_iso3s)} ISO3s...")
-    for idx, row in df_iso3s.iterrows():
-        iso3 = df_iso3s.loc[idx, "iso_3"]
-        shp_url = df_iso3s.loc[idx, "o_shp"]
-        max_adm = df_iso3s.loc[idx, "max_adm_level"]
-        logger.info(f"Processing data for {iso3}...")
-
-        with tempfile.TemporaryDirectory() as td:
-            load_shp(shp_url, td, iso3)
-            gdf = gpd.read_file(f"{td}/{iso3.lower()}_adm0.shp")
-            try:
-                ds_clipped = prep_raster(ds, gdf)
-            except Exception as e:
-                logger.error(f"Error preparing raster for {iso3}: {e}")
-                stack_trace = traceback.format_exc()
-                insert_qa_table(iso3, None, dataset, e, stack_trace, engine)
-                continue
-
-            # Loop through each adm
-            for adm_level in list(range(0, max_adm + 1)):
-                try:
-                    gdf = gpd.read_file(f"{td}/{iso3.lower()}_adm{adm_level}.shp")
-                    logger.info(f"Computing stats for adm{adm_level}...")
-                    fast_zonal_stats_runner(
-                        ds_clipped,
-                        gdf,
-                        adm_level,
-                        iso3,
-                        save_to_database=False,
-                        engine=engine,
-                        dataset=dataset,
-                    )
-                except Exception as e:
-                    logger.error(
-                        f"Error calculating stats for {iso3} at {adm_level}: {e}"
-                    )
-                    stack_trace = traceback.format_exc()
-                    insert_qa_table(iso3, adm_level, dataset, e, stack_trace, engine)
-                    continue
+    for start_date, end_date in date_ranges:
+        process_chunk(start_date, end_date, dataset, args, engine, df_iso3s)
 
     logger.info("Done calculating and saving stats.")

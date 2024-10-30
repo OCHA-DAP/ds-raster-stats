714af349caffa5c3d43b7c954d2c6541677eec87
hannahker <hannah.ker96@gmail.com>
2024-10-30 13:45:29 -0400

    WIP fix

--- src/utils/raster_utils.py
+++ src/utils/raster_utils.py
@@ -304,7 +304,7 @@ def prep_raster(ds, gdf_adm, logger=None):
 
     logger.debug("Clipping raster to iso3 bounds and persisting in memory...")
     minx, miny, maxx, maxy = gdf_adm.total_bounds
-    ds_clip = ds.sel(x=slice(minx, maxx), y=slice(maxy, miny)).persist()
+    ds_clip = ds.sel(x=slice(minx - 1, maxx + 1), y=slice(maxy + 1, miny - 1)).persist()
     logger.debug("Upsampling raster...")
     ds_resampled = upsample_raster(ds_clip, logger=logger)
     logger.debug("Raster prep completed.")
